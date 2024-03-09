9285d1ac52df1224018690bb3a6715276f1fc344
ecattrina <katy_060104@mail.ru>
2024-03-09 11:35:57 +0500

    Создание класса "SquareTrapecia"

---
+++ TRPO-Lab3/TRPO-Lab3.Library/SquareTrapecia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_Lab3.Library
+{
+    public class SquareTrapecia
+    {
+        public double Calculate( double base1, double base2, double height )
+        {
+            double area = 1 / 2 * (base1 + base2) * height;
+            return area;
+        }
+    }
+}
