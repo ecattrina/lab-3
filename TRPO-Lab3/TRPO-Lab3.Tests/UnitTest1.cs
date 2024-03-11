8cc66e364429b5661c168a0b18e947d29aecbd42
ecattrina <katy_060104@mail.ru>
2024-03-11 14:47:05 +0500

    Изменение содержания Теста Test1

--- TRPO-Lab3/TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3/TRPO-Lab3.Tests/UnitTest1.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TRPO_Lab3.Tests
 {
     public class UnitTest1
 
-    {
+    {   [Test]
         public void Test1()
         {
             const double base1 = 1;
             const double base2 = 2;
             const double height = 1;
 
+            const double expected = 1.5; 
+
             var result = TRPO_Lab3.Library.SquareTrapecia.Calculate(base1, base2, height);
 
-
+            Assert.AreEqual(expected, result);
         }
     }
 }
